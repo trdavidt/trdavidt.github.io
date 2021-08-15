1ca30be7e7f7c492eb950fca49cc8527394584b3
David Tran <tr.davidt@gmail.com>
2021-08-15 11:21:18 -0700

    Responsive fixes

--- _projects/anojs.md
+++ _projects/anojs.md
@@ -4,6 +4,6 @@ title: "Ano.js contributor"
 skills: ["HTML", "CSS", "JavaScript"]
 date: 2020-2-31
 image: /images/websites/anojs-desktop.png
-link: "https://ampheros.com"
+link: "https://anojs.com"
 desc: "Contributor for open-source animation library."
 ---
--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 /* Toggle nav on scroll/click */
 navContainer = document.querySelector(".nav-container");
-navLinks = document.querySelector(".nav-container .nav-links")
+navLinks = document.querySelectorAll(".nav-container .nav-link");
 navHeight = document.querySelector(".nav-container").clientHeight;
+navToggle = document.getElementById("toggle-btn");
+navOpened = false;
 
 onScrollNav = function() {  
-  if (window.pageYOffset >= navHeight) {
+  if (window.pageYOffset > navHeight) {
     navContainer.style.transition = "padding .5s ease";
     navContainer.style.padding = "1% 5%";    
+    navContainer.style.backgroundColor = "rgba(14, 41, 60, .9)";
   }
   else {
     navContainer.style.padding = "2% 5%";
+    navContainer.style.opacity = "1";
+    navContainer.style.backgroundColor = "rgb(14, 41, 60)";
   }  
 }
 
-onClickNav = function() {
-
+onClickNav = function() {  
+  if(!navOpened) {   
+    navLinks.forEach(navLink => {
+      navLink.style.position = "relative"; 
+      navLink.style.opacity = 1;     
+      navLink.style.transition = "opacity .25s ease";
+    });
+    navToggle.innerHTML = "<i class='fas fa-angle-right'></i>";
+  }
+  else {
+    navLinks.forEach(navLink => {
+      navLink.style.position = "absolute";
+      navLink.style.opacity = 0; 
+      navLink.style.transition = "opacity 0s ease"; 
+    });    
+    navToggle.innerHTML = "<i class='fas fa-bars'></i>";
+  }
+  navOpened = !navOpened
 }
 
 window.addEventListener("scroll", onScrollNav);
+navToggle.addEventListener("click", onClickNav)
 
+/* Scroll to anchor */
+anchors = document.querySelectorAll(".anchor");
+anchors.forEach(anchor => {
+  anchor.style.display = "inline-block";
+  anchor.style.position = "relative";
+  anchor.style.top = `-${navHeight + 30}px`;
+});
